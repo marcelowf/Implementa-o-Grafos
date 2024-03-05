351d53e7fa3b2c10fd99ad976300410f7cd9eb01
marcelowf <marcelowzorekfilho@gmail.com>
2024-03-05 18:30:08 -0300

    Pa

--- src/br/com/tde/lista_adjacências/ListaDeAdjacencias.java
+++ src/br/com/tde/lista_adjacências/ListaDeAdjacencias.java
@@ -6,80 +6,125 @@ import java.util.List;
 import br.com.tde.node.Node;
 
 public class ListaDeAdjacencias {
-    private boolean direcionado;
-    private boolean ponderado;
-    private List<Node> nodes;
+	private boolean direcionado;
+	private boolean ponderado;
+	private List<Node> nodes;
 
-    public ListaDeAdjacencias(boolean ponderado, boolean direcionado) {
-        this.direcionado = direcionado;
-        this.ponderado = ponderado;
-        this.nodes = new ArrayList<>();
-    }
+	public ListaDeAdjacencias(boolean ponderado, boolean direcionado) {
+		this.direcionado = direcionado;
+		this.ponderado = ponderado;
+		this.nodes = new ArrayList<>();
+	}
 
-    public boolean adicionarNode(String valorNode) {
-        Node nodeParaAdicionar = getNodeByValue(valorNode);
+	public boolean adicionarNode(String valorNode) {
+		Node nodeParaAdicionar = getNodeByValue(valorNode);
 
-        if (nodeParaAdicionar == null) {
-            Node novoNode = new NodeListaDeAdjacencias(valorNode, ponderado, direcionado);
-            
-            nodes.add(novoNode);
-            return true;
-        }
-        return false;
-    }
-    
-    public boolean deletarNode(String valorNode) {
-        Node nodeParaRemover = getNodeByValue(valorNode);
+		if (nodeParaAdicionar == null) {
+			Node novoNode = new NodeListaDeAdjacencias(valorNode, ponderado, direcionado);
 
-        if (nodeParaRemover != null) {
-            for (Node node : nodes) {
-                if (node != nodeParaRemover) {
-                    ((NodeListaDeAdjacencias) node).removerRelacao(valorNode);
-                }
-            }
-            nodes.remove(nodeParaRemover);
-            return true;
-        }
-        return false;
-    }
+			nodes.add(novoNode);
+			return true;
+		}
+		return false;
+	}
 
-    
-    public boolean adicionarRelacao(String valorOrigem, String valorDestino, int peso) {
-        Node origem = getNodeByValue(valorOrigem);
-        Node destino = getNodeByValue(valorDestino);
-        
-        if (origem != null && destino != null) {
-            ((NodeListaDeAdjacencias) origem).adicionarRelacao(valorDestino, peso);
-            
-            if (!direcionado) {
-            	((NodeListaDeAdjacencias) destino).adicionarRelacao(valorOrigem, peso);
-            }
-            return true;
-        }
-        return false;
-    }
+	public boolean deletarNode(String valorNode) {
+		Node nodeParaRemover = getNodeByValue(valorNode);
 
-    public boolean removerRelacao(String valorOrigem, String valorDestino) {
-        Node origem = getNodeByValue(valorOrigem);
-        Node destino = getNodeByValue(valorDestino);
-        
-        if (origem != null && destino != null) {
-        	((NodeListaDeAdjacencias) origem).removerRelacao(valorDestino);
-            
-            if (!direcionado) {
-            	((NodeListaDeAdjacencias) destino).removerRelacao(valorOrigem);
-            }
-            return true;
-        }
-        return false;
-    }
+		if (nodeParaRemover != null) {
+			for (Node node : nodes) {
+				if (node != nodeParaRemover) {
+					((NodeListaDeAdjacencias) node).removerRelacao(valorNode);
+				}
+			}
+			nodes.remove(nodeParaRemover);
+			return true;
+		}
+		return false;
+	}
 
-    private Node getNodeByValue(String valorNode) {
-        for (Node node : nodes) {
-            if (node.getValorNode().equals(valorNode)) {
-                return node;
-            }
-        }
-        return null;
-    }
+	public boolean adicionarRelacao(String valorOrigem, String valorDestino, int peso) {
+		Node origem = getNodeByValue(valorOrigem);
+		Node destino = getNodeByValue(valorDestino);
+
+		if (origem != null && destino != null) {
+			((NodeListaDeAdjacencias) origem).adicionarRelacao(valorDestino, peso);
+
+			if (!direcionado) {
+				((NodeListaDeAdjacencias) destino).adicionarRelacao(valorOrigem, peso);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public boolean removerRelacao(String valorOrigem, String valorDestino) {
+		Node origem = getNodeByValue(valorOrigem);
+		Node destino = getNodeByValue(valorDestino);
+
+		if (origem != null && destino != null) {
+			((NodeListaDeAdjacencias) origem).removerRelacao(valorDestino);
+
+			if (!direcionado) {
+				((NodeListaDeAdjacencias) destino).removerRelacao(valorOrigem);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public boolean verificarRelacao(String valorOrigem, String valorDestino) {
+		Node origem = getNodeByValue(valorOrigem);
+		Node destino = getNodeByValue(valorDestino);
+		if (origem != null && destino != null) {
+			((NodeListaDeAdjacencias) origem).relacaoExistente(valorDestino);
+			return true;
+		}
+		return false;
+	}
+
+	public int CalculoIndegree(String valorNode) {
+		return -1;
+	}
+
+	public int CalculoOutdegree(String valorNode) {
+		return -1;
+	}
+
+	public int CalculoDegree(String valorNode) {
+		return -1;
+	}
+
+	public boolean definirAtualizarPeso(String valorOrigem, String valorDestino) {
+		return true;
+	}
+
+	public double RecuperarPeso(String valorOrigem, String valorDestino) {
+		return -1.0;
+	}
+
+	// plotar
+
+	private Node getNodeByValue(String valorNode) {
+		for (Node node : nodes) {
+			if (node.getValorNode().equals(valorNode)) {
+				return node;
+			}
+		}
+		return null;
+	}
+
+//	public void printNodes() {
+//		for (Node node : nodes) {
+//			System.out.println("Node: " + node.getValorNode());
+//		}
+//	}
+//
+//	public void printRelacoes() {
+//		for (Node node : nodes) {
+//			System.out.println("Node: " + node.getValorNode());
+//			Object relacoes = ((NodeListaDeAdjacencias) node).getRelacoes();
+//			System.out.println(relacoes);
+//		}
+//	}
 }
--- src/br/com/tde/main/Main.java
+++ src/br/com/tde/main/Main.java
@@ -6,7 +6,28 @@ import br.com.tde.matriz_adjacências.MatrizDeAdjacencias;
 public class Main {
 
 	public static void main(String[] args) {
-	    
+		ListaDeAdjacencias lista = new ListaDeAdjacencias(true, true);
+
+	    // Adiciona alguns nós
+	    lista.adicionarNode("A");
+	    lista.adicionarNode("B");
+	    lista.adicionarNode("C");
+
+	    // Adiciona algumas relações
+	    lista.adicionarRelacao("A", "B", 1);
+	    lista.adicionarRelacao("B", "C", 2);
+	    lista.adicionarRelacao("C", "A", 3);
+	    if(lista.verificarRelacao("A", "B")) {
+	    	System.out.println("OK");
+	    }
+//	    lista.printNodes();
+//	    lista.printRelacoes();
+//	    
+//	    lista.deletarNode("A");
+//	    lista.removerRelacao("B", "C");
+//
+//	    lista.printNodes();
+//	    lista.printRelacoes();
 	}
 
 	
